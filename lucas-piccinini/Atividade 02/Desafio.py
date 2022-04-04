f084c22c19ecbd7d2c72491a452a48caf08656a1
Senp <101899873+SenpSenp@users.noreply.github.com>
2022-04-04 19:24:34 -0300

    Update Desafio.py

--- lucas-piccinini/Atividade 02/Desafio.py
+++ lucas-piccinini/Atividade 02/Desafio.py
@@ -1,16 +1,16 @@
-#pedra papel tesoura, 1 jogada, 2 jogadores
+#pedra papel tesoura, 1 jogada, 2 jogadores, jogadas por nuúmeros
 
-print("Pedra, papel, tesoura, digite sua jogada com a primeira letra maiúscula")
-jogador1 = input("jogador1, digite sua jogada: ")
-jogador2 = input("jogador2, digite sua jogada: ")
+print("Pedra, papel, tesoura, digite sua jogada seguindo o padrâo: 1 = Pedra, 2 = Papel, 3 = Tesoura")
+jogador1 = int(input("jogador1, digite sua jogada: "))
+jogador2 = int(input("jogador2, digite sua jogada: "))
 
-if jogador1 == "Pedra" and jogador2 == "Pedra" or jogador1 == "Papel" and jogador2 == "Papel" or jogador1 == "Tesoura" and jogador2 == "Tesoura":
+if jogador1 == 1 and jogador2 == 1 or jogador1 == 2 and jogador2 == 2 or jogador1 == 3 and jogador2 == 3:
     print("Empate")
 else:
-    if jogador1 == "Pedra" and jogador2 =="Tesoura" or jogador1 == "Tesoura" and jogador2 == "Papel" or jogador1 == "Papel" and jogador2 == "Pedra":
+    if jogador1 == 1 and jogador2 == 3 or jogador1 == 3 and jogador2 == 2 or jogador1 == 2 and jogador2 == 1:
         print("Jogador 1 Venceu")
     else:
-        if jogador2 == "Pedra" and jogador1 == "Tesoura" or jogador2 == "Tesoura" and jogador1 == "Papel" or jogador2 == "Papel" and jogador1 == "Pedra":
+        if jogador2 == 1 and jogador1 == 3 or jogador2 == 3 and jogador1 == 2 or jogador2 == 2 and jogador1 == 1:
             print("Jogador 2 Venceu")
         else:
             print("Jogada inválida, tentar novamente.")
