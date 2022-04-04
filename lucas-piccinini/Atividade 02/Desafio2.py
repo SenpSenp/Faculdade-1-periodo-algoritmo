6826de925ea4f0efa706d6e85d684b1ddfbb53fc
Senp <101899873+SenpSenp@users.noreply.github.com>
2022-04-04 20:00:29 -0300

    Update Desafio2.py

--- lucas-piccinini/Atividade 02/Desafio2.py
+++ lucas-piccinini/Atividade 02/Desafio2.py
@@ -1,4 +1,4 @@
-#pedra papel tesoura, 1 jogada, 2 jogadores
+#pedra papel tesoura, 1 jogador vs máquina, 1 jogada
 
 print("Pedra, papel, tesoura, digite sua jogada seguindo o padrâo: 1 = Pedra, 2 = Papel, 3 = Tesoura")
 jogador1 = int(input("jogador1, digite sua jogada: "))
@@ -6,16 +6,31 @@ jogador1 = int(input("jogador1, digite sua jogada: "))
 from random import *
 jogada = (randint(0, 2))
 
-if jogador1 == 1 and jogada == 0 or jogador1 == 2 and jogada == 1 or jogador1 == 3 and jogada == 2:
-    print(f"Máquina jogou {jogada+1}")
-    print("Empate")
+if jogador1 == 1 and jogada == 0:
+    print("Máquina jogou Pedra, empate")
 else:
-    if jogador1 == 1 and jogada == 2 or jogador1 == 3 and jogada == 1 or jogador1 == 2 and jogada == 0:
-        print(f"Máquina jogou {jogada + 1}")
-        print("Jogador 1 Venceu")
+    if jogador1 == 2 and jogada == 1:
+        print("Máquina jogou Papel, empate")
     else:
-        if jogada == 0 and jogador1 == 3 or jogada == 2 and jogador1 == 2 or jogada == 1 and jogador1 == 1:
-            print(f"Máquina jogou {jogada + 1}")
-            print("Jogador 2 Venceu")
+        if jogador1 == 3 and jogada == 2:
+            print("Máquina jogou Tesoura, empate")
         else:
-            print("Jogada inválida, tentar novamente.")
+            if jogador1 == 1 and jogada == 2:
+                print("Máquina jogou Tesoura, Você venceu!")
+            else:
+                if jogador1 == 2 and jogada == 0:
+                    print("Máquina jogou Pedra, Você venceu!")
+                else:
+                    if jogador1 == 3 and jogada == 1:
+                        print("Máquina jogou Papel, Você venceu!")
+                    else:
+                        if jogador1 == 1 and jogada == 1:
+                            print("Máquina jogou Papel, Você perdeu :c")
+                        else:
+                            if jogador1 == 2 and jogada == 2:
+                                print("Máquina jogou Tesoura, Você perdeu :c")
+                            else:
+                                if jogador1 == 3 and jogada == 0:
+                                    print("Máquina jogou Pedra, Você perdeu :c")
+                                else:
+                                    print("Jogada inválida, tente novamente")
