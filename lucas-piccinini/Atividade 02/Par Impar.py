2366ab00d1b8eafd50d711d528e5f3b7750c29f8
Senp <101899873+SenpSenp@users.noreply.github.com>
2022-04-11 20:30:07 -0300

    Update Par Impar.py

--- lucas-piccinini/Atividade 02/Par Impar.py
+++ lucas-piccinini/Atividade 02/Par Impar.py
@@ -2,19 +2,18 @@
 
 from random import *
 
-modo_de_jogo = int(input("Deseja jogar sozinho ou com um amigo? (sozinho = 0 / com amigo = 1)"))
-tipo = int(input("Deseja Par ou impar? (par = 0 / impar = 1)"))
-jogador1 = int(input("Jogador 1, escolha um número:"))
-
-if modo_de_jogo < 0 or modo_de_jogo > 1:
+modo_jogo = int(input("Deseja jogar sozinho ou com um amigo? (sozinho = 0 / com amigo = 1)"))
+if modo_jogo < 0 or modo_jogo > 1:
     print("Modo de jogo inválido")
 else:
-    if modo_de_jogo == 0:
-        jogador2 = (randint(0, 1))
-        resultado = (jogador1 + jogador2) % 2
-        if tipo < 0 or tipo > 1:
-            print("Apenas é aceito 0 e 1 como modo")
-        else:
+    tipo = int(input("Deseja Par ou impar? (par = 0 / impar = 1)"))
+    if tipo < 0 or tipo > 1:
+        print("Apenas é aceito 0 e 1 como modo")
+    else:
+        jogador1 = int(input("Jogador 1, escolha um número:"))
+        if modo_jogo == 0:
+            jogador2 = (randint(0, 1))
+            resultado = (jogador1 + jogador2) % 2
             if tipo == 0:
                 if resultado == 0:
                     vencedor = "Jogador 1"
@@ -25,19 +24,19 @@ else:
                     vencedor = "Máquina"
                 else:
                     vencedor = "Jogador 1"
-    else:
-        jogador2 = int(input("Jogador 2, escolha um número:"))
-        resultado = (jogador1 + jogador2) / 2
-        if tipo == 0:
-            if resultado == 0:
-                vencedor = "Jogador 1"
-            else:
-                vencedor = "Jogador 2"
         else:
-            if resultado == 0:
-                vencedor = "Jogador 2"
+            jogador2 = int(input("Jogador 2, escolha um número:"))
+            resultado = (jogador1 + jogador2) / 2
+            if tipo == 0:
+                if resultado == 0:
+                    vencedor = "Jogador 1"
+                else:
+                    vencedor = "Jogador 2"
             else:
-                vencedor = "Jogador 1"
+                if resultado == 0:
+                    vencedor = "Jogador 2"
+                else:
+                    vencedor = "Jogador 1"
 
-print(f"{jogador1} + {jogador2} = {jogador1+jogador2:.2f}")
-print(f"Vencedor: {vencedor}")
+        print(f"{jogador1} + {jogador2} = {jogador1+jogador2:.0f}")
+        print(f"Vencedor: {vencedor}")
